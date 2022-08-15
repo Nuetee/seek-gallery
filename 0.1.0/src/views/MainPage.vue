94bf61cb0bca18a421bab64c763a4a85bf00b301
Nuetee <sungjoon729@gmail.com>
2022-08-15 18:31:36 +0900

    architecture 추가. MainPage MainHeader 수정 중.

---
+++ 0.1.0/src/views/MainPage.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="mainPage">
+        <MainHeader :background-vue="'white'">
+            <template v-slot:default>
+                <img src="../assets/seek_logo.png">
+            </template>
+        </MainHeader>
+        <div class="body">
+            <div class="poster"></div>
+            <div class="exhibitionTitle">
+                <TitleHeader></TitleHeader>
+            </div>
+            <div class="exhibitionArtworks">
+                <TitleHeader></TitleHeader>
+                <ArtworkTrackList></ArtworkTrackList>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import MainHeader from '@/widgets/MainHeader.vue';
+import TitleHeader from '@/widgets/TitleHeader.vue';
+import ArtworkTrackList from '@/widgets/ArtworkTrackList.vue';
+    export default {
+        name: 'MainPage',
+        components: {
+            MainHeader,
+            TitleHeader,
+            ArtworkTrackList
+        },
+        data() {
+            return {
+                sampleData: ''
+            };
+        },
+        beforeCreate() {},
+        created() {},
+        beforeMount() {},
+        mounted() {},
+        beforeUpdate() {},
+        updated() {},
+        beforeUnmount() {},
+        unmounted() {},
+        methods: {}
+    }
+</script>
+<style lang="scss" scoped src="../scss/MainPage/mainPage.scss"></style>
