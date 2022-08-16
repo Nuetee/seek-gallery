6fae6139b58cdf652f943448907c64f9db3a2dcf
Nuetee <sungjoon729@gmail.com>
2022-08-16 16:51:33 +0900

    mainPage

--- 0.1.0/src/views/MainPage.vue
+++ 0.1.0/src/views/MainPage.vue
@@ -6,26 +6,30 @@
             </template>
         </MainHeader>
         <div class="body">
-            <div class="poster">
-                <img src="../assets/766f3bd97f3c9cbc47e7347ba6c665c2.jpg">
+            <div class="top">
+                <div class="poster">
+                    <img class="posterImage" @load="this.setTopHeight()" src="../assets/ee8eabd8df23a4f9b29100d59217586e.jpeg">
+                </div>
+                <div class="exhibitionTitle">
+                    <TitleHeader></TitleHeader>
+                </div>
             </div>
-            <div class="ghost">
+            <div class="bottom">
+                <div class="exhibitionIntroduction">
 
-            </div>
-            <div class="exhibitionTitle">
-                <TitleHeader></TitleHeader>
-            </div>
-            <div class="exhibitionArtworks">
-                <TitleHeader></TitleHeader>
-                <ArtworkTrackList></ArtworkTrackList>
+                </div>
+                <div class="exhibitionArtworks">
+                    <TitleHeader></TitleHeader>
+                    <ArtworkTrackList></ArtworkTrackList>
+                </div>
             </div>
         </div>
     </div>
 </template>
 <script>
-import MainHeader from '@/widgets/MainHeader.vue';
-import TitleHeader from '@/widgets/TitleHeader.vue';
-import ArtworkTrackList from '@/widgets/ArtworkTrackList.vue';
+    import MainHeader from '@/widgets/MainHeader.vue';
+    import TitleHeader from '@/widgets/TitleHeader.vue';
+    import ArtworkTrackList from '@/widgets/ArtworkTrackList.vue';
     export default {
         name: 'MainPage',
         components: {
@@ -35,18 +39,48 @@ import ArtworkTrackList from '@/widgets/ArtworkTrackList.vue';
         },
         data() {
             return {
-                sampleData: ''
+                exhibition_title: null,
+                is_fixed: false
             };
         },
         beforeCreate() {},
-        created() {},
+        created() {
+            window.addEventListener('scroll', this.scrollEvent)
+        },
         beforeMount() {},
-        mounted() {},
+        mounted() {
+            this.exhibition_title = document.getElementsByClassName('exhibitionTitle')[0]
+        },
         beforeUpdate() {},
-        updated() {},
+        updated() {
+        },
         beforeUnmount() {},
         unmounted() {},
-        methods: {}
+        methods: {
+            setTopHeight () {
+                let poster_image = document.getElementsByClassName('posterImage')[0]
+                let top = document.getElementsByClassName('top')[0]
+
+                let padding_bottom = window.innerWidth * 0.5
+                if (window.innerWidth >= 480) {
+                    padding_bottom = 480 * 0.5
+                }
+
+                top.style.setProperty('height', `${poster_image.clientHeight + padding_bottom}px`)
+            },
+            scrollEvent () {
+                if (this.exhibition_title.getBoundingClientRect().bottom < window.innerHeight && !this.is_fixed) {
+                    this.exhibition_title.style.setProperty('position', 'fixed')
+                    this.exhibition_title.style.setProperty('bottom', '0')
+                    this.exhibition_title.style.setProperty('left', '0')
+                    this.is_fixed = true
+                }
+                // else {
+                //     console.log('else')
+                //     this.exhibition_title.style.setProperty('position', 'static')
+                // }
+            }
+        }
     }
 </script>
 <style lang="scss" scoped src="../scss/MainPage/mainPage.scss"></style>
