7b5b6a0c58e55bd036c011ff8f997cb56058de15
young-rae-kim <dufo7070@kaist.ac.kr>
2022-08-16 14:42:32 +0900

    Add exhibition class

--- 0.1.0/src/views/HomeView.vue
+++ 0.1.0/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img @click="this.test" alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +8,17 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { Exhibition } from '@/classes/exhibition'
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
+  },
+  methods: {
+    async test () {
+      const ex = await new Exhibition('1').init()
+    }
   }
 }
 </script>
