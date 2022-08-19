411538325fb1ac92917ff18311020c5017a868d8
Nuetee <sungjoon729@gmail.com>
2022-08-19 19:01:55 +0900

    MainPage 잠정 완료.

--- 0.1.0/src/views/ArtworkPage.vue
+++ 0.1.0/src/views/ArtworkPage.vue
@@ -1,17 +1,52 @@
-<template></template>
+<template>
+    <div id="artworkPage" v-if="this.artwork">
+        <MainHeader :background_color="'transparent'">
+            <template v-slot:default>
+                <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7 13L1 7L7 1" stroke="black" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                </svg>
+            </template>
+        </MainHeader>
+        <div class="body">
+            <div class="top">
+                <div class="artworkTitle poppins">{{ this.artwork.getName() }}</div>
+                <ArchiveInfo :archive_count="this.artwork.getArchiveCount()"></ArchiveInfo>
+            </div>
+            <div class="bottom">
+                <div class="artist">{{ this.artwork.getArtist().nickname }}</div>
+                <SNSLink :sns_link="this.artwork.getArtist().getSNS()"></SNSLink>
+            </div>
+        </div>
+    </div>
+</template>
 <script>
+    import MainHeader from '@/widgets/MainHeader.vue';
+    import ArchiveInfo from '@/widgets/ArchiveInfo.vue';
+    import SNSLink from '@/widgets/SNSLink.vue';
+
+    import { Artwork } from '@/classes/artwork';
+
     export default {
         name: 'ArtworkPage',
-        components: {},
+        components: { MainHeader, ArchiveInfo, SNSLink },
         data() {
             return {
-                sampleData: ''
+                id: this.$route.query.id,
+                artwork: null,
             };
         },
-        beforeCreate() {},
-        created() {},
+        async created() {
+            this.artwork = await new Artwork(this.id).init()
+        },
         beforeMount() {},
-        mounted() {},
+        mounted() {
+            // const _this = this
+            // setTimeout(function() {
+            //     window.scrollTo(0, 1000)
+            // }, 100)
+            
+        },
         beforeUpdate() {},
         updated() {},
         beforeUnmount() {},
@@ -19,3 +54,4 @@
         methods: {}
     }
 </script>
+<style lang="scss" scoped src="../scss/ArtworkPage/artworkPage.scss"></style>
