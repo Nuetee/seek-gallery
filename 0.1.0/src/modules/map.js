f95113e34a089753901ee92b0ddefb441c7fa2e5
young-rae-kim <dufo7070@kaist.ac.kr>
2022-10-26 21:00:44 +0900

    Implement exhibition fetching by area

---
+++ 0.1.0/src/modules/map.js
@@ -0,0 +1,31 @@
+// Deploying modules
+import axios from 'axios'
+import { sendRequest } from '../modules/api'
+
+if (process.env.NODE_ENV === 'production') {
+    axios.defaults.baseURL = process.env.VUE_APP_SERVER_URL
+}
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
+
+// Area string rules
+// 한남: 'hannam'
+// 이태원: 'itaewon'
+// 성수: 'seongsu'
+// 북촌: 'bukcheon'
+// 논현: 'nonhyeon'
+// 연남: 'yeonnam'
+// 을지로: 'uljiro'
+// 문래: 'munrae'
+export async function getExhibitionsOnArea (area, offset, limit) {
+    const { status, data } = await sendRequest('post', '/exhibition/area', {
+        area : area,
+        offset : offset,
+        limit : limit
+    })
+    if (status < 500) {
+        return data[0].map(x => x.page_id)
+    }
+    else {
+        return []
+    }
+}
