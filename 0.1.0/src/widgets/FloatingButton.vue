a7e8565bdb26a46f59977d0a535d8ff90fce774e
Nuetee <sungjoon729@gmail.com>
2022-12-16 19:01:46 +0900

    clicked button, list click 이벤트 추가, 새로운 eye(Eye.vue) 및 eyeroll animation 추가

---
+++ 0.1.0/src/widgets/FloatingButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="FloatingButton poppins" ref="floatingButton" @click="this.clickFloatingButton()">
+        <div class="phrase" v-if="this.is_clicked">
+            Love it
+        </div>
+        <Eyes v-else></Eyes>
+    </div>
+</template>
+<script>
+
+import Eyes from './Eyes.vue';
+
+export default {
+    name: 'FloatingButton',
+    components: {
+        Eyes
+    },
+    data() {
+        return {
+            is_clicked: true,
+        };
+    },
+    methods: {
+        stopPropagation(event) {
+            // event 전파 방지
+            if (event.stopPropagation) event.stopPropagation();
+            else event.cancelBubble = true; // IE 대응
+        },
+        clickFloatingButton () {
+            this.$emit('click-floating-button', this.is_clicked)
+            this.is_clicked = !this.is_clicked
+        }
+    }
+}
+</script>
+<style lang="scss" scoped src="../scss/widgets/floatingButton.scss"></style>
